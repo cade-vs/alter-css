5bd94814c2d62d443fc1b6df887a9960ca67cb9b
Vladi Belperchinov-Shabanski <cade@bis.bg>
2021-04-13 01:23:26 +0300

    cleanup

--- alter-css.pl
+++ alter-css.pl
@@ -58,8 +58,6 @@ sub line_set_var
 
   $VARS{ uc $1 } = [ $args, split /\s+/, $args ];
 
-#print STDERR "DEBUG: SET VAR: $1 -- $2\n";
-
   return 1;
 }
 
@@ -68,8 +66,6 @@ sub line_set_block
   my $line = shift;
   my $data = shift;
 
-#print STDERR ">>>>>> [$line]\n";
-  
   return undef unless $line =~ /^\$\$([a-z_0-9-]+)/i;
   my $name = uc $1;
   my @block;
@@ -81,8 +77,6 @@ sub line_set_block
   
   $BLOCKS{ $name } = \@block;
 
-#print STDERR "DEBUG: SET BLOCK: $name -- \n" . join( "\n", @block ) . "\n^^^\n";
-
   return 1;
 }
 
@@ -92,8 +86,6 @@ sub line_print_block
   return undef unless $line =~ /^\s+\$\$([a-z_0-9-]+)(\s+(.*?)\s*)?$/i;
   my $name = uc $1;
   my $args =    update_vars( $3 );
-
-#print STDERR "DEBUG: GET BLOCK: $name -- $args\n";
 
   if( exists $BLOCKS{ $name } )
     {
@@ -114,7 +106,6 @@ sub update_vars
   my $args = shift;
 
   $line =~ s/\$([a-z_0-9-]+)(\.(\d+))?/__get_var( $1, $3, $args )/gie;
-  
   return $line;
 }
 
@@ -124,19 +115,7 @@ sub __get_var
   my $var_idx  =    shift || 0;
   my $args     =    shift || [];
 
-#print STDERR "DEBUG: GET VAR: $var_name -- $var_idx (@$args)\n";
-  if( $var_name =~ /^\d+$/ )
-    {
-    return $args->[ $var_name ];
-    }
-
-  if( exists $VARS{ $var_name } )
-    {
-    return $VARS{ $var_name }[ $var_idx ];
-    }
-  else
-    {
-    # TODO: warning: unknown var name. error? fatal?
-    return undef;
-    }  
+  return $args->[ $var_name ] if$var_name =~ /^\d+$/;
+  return $VARS{ $var_name }[ $var_idx ] if exists $VARS{ $var_name };
+  return undef; # TODO: warning: unknown var name. error? fatal?
 }
